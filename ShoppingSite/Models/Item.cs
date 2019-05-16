5598ceff1ad5e3cb54fb5e09d581642fbd621322
Kamm Freudenstein <kammfreudenstein@gmail.com>
2019-05-16 11:46:20 -0700

    Item Class created

--- ShoppingSite/Models/Item.cs
+++ ShoppingSite/Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,18 @@ namespace ShoppingSite.Models
 {
 	public class Item
 	{
+		[Key]
+		public int ItemID { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+		[Required]
+		public string Category { get; set; }
+
+		[Required]
+		public double Price { get; set; }
+
+
 	}
 }
